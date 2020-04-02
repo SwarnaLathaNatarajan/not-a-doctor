3c899a1752491a9cc80854936d17856a0aa25f6d
Aditi <ap2205@gmail.com>
2020-04-01 19:04:25 -0600

    added base class code to write data into json

---
+++ resource-tracker/base.py
@@ -0,0 +1,56 @@
+import csv
+import json
+import os
+
+"""
+store-name
+Image
+product name
+product url
+description
+price
+available or not 
+"""
+
+class Store:
+    def __init__(self):
+        self.store_data = dict()
+        self.store_name = ''
+        self.title = ''
+        self.image_url = ''
+        self.product_url = ''
+        self.description = ''
+        self.price = ''
+        self.stock = None
+    
+    def add_item(self):
+        self.store_data['store-name'] = self.store_name
+        self.store_data['product-title'] = self.title
+        self.store_data['image-url'] = self.image_url
+        self.store_data['product-url'] = self.product_url
+        self.store_data['product-description'] = self.description
+        self.store_data['price'] = self.price
+        self.store_data['stock'] = self.stock
+        #write into json
+        self.write_into_json()
+
+    def write_into_json(self):
+        # Check if file-path available
+        file_path = '../data/'
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        file_name = os.path.join(file_path ,'store-data.json')
+        with open(file_name, 'a') as outfile:
+            if outfile.tell() == 0:
+                outfile.write('[')
+                json.dump(self.store_data, outfile)
+                outfile.write(']')
+
+            else:
+                outfile.seek(0, os.SEEK_END)
+                outfile.seek(outfile.tell() - 1, os.SEEK_SET)
+                outfile.truncate()
+                outfile.write(',')
+                json.dump(self.store_data, outfile)
+                outfile.write(']')
+
