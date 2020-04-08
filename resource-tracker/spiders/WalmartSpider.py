6319af006a39f5130a31b3a3cdd1eecbc4641837
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-08 12:44:36 -0600

    Walmart spider class done

--- resource-tracker/spiders/WalmartSpider.py
+++ resource-tracker/spiders/WalmartSpider.py
@@ -22,10 +22,12 @@ class Walmart:
         containers = page_soup.find_all("div",{"class":"search-result-gridview-item-wrapper"})
         print(len(containers))
         for container in containers:
-            store = Store()
-            store.store_name = 'Walmart'
-            store.title = container.img["alt"]
-            store.image_url = container.img["data-image-src"]
-            store.product_url =self.url+container.a["href"]
-            store.price =container.find_all("span", {"class": "visuallyhidden"})[-1].text
-            store.add_item()
+            out_of_stock = len(containers[0].find_all("div",{"class":"product-sub-title-block product-out-of-stock"}))!=0
+            if not out_of_stock:
+                store = Store()
+                store.store_name = 'Walmart'
+                store.title = container.img["alt"]
+                store.image_url = container.img["data-image-src"]
+                store.product_url =self.url+container.a["href"]
+                store.price =container.find_all("span", {"class": "visuallyhidden"})[-1].text
+                store.add_item()
