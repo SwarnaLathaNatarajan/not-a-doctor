365578111f8ddc7e45583b887b343bb0ecf1821f
Aditi <ap2205@gmail.com>
2020-04-18 22:18:46 -0600

    components front-end

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,43 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Layout, Header, Navigation, Content} from 'react-mdl';
+import { Link } from 'react-router-dom';
+import Main from './components/main';
+function App() {
+  return (
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <Link to="/module1">Module1</Link>
+                <Link to="/module2">Module2</Link>
+                <Link to="/module3">Module3</Link>
+                <Link to="/module4">Module4</Link>
+            </Navigation>
+        </Header>
+        {/* <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer> */}
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/count-info")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.apiResponse}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  );
 }
 
 export default App;
