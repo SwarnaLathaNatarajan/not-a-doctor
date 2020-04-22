d3dc00629fbf21c7dbf8f02fc55bc0b50d92960c
Keerthika Rajvel <keerthikarajvel@gmail.com>
2020-04-22 15:29:55 -0600

    World map added

--- front-end-app/src/components/Trendchart.jsx
+++ front-end-app/src/components/Trendchart.jsx
@@ -4,8 +4,112 @@ import Plot from "react-plotly.js";
 class Trendchart extends React.Component {
   state = {};
   render() {
+    var confirmed = [
+      320,
+      3095,
+      26,
+      737,
+      1724,
+      20126,
+      257,
+      118,
+      255,
+      11,
+      5,
+      12715,
+    ];
+    var deaths = [320, 3095, 26, 737, 1724, 20126, 257, 118, 255, 11, 5, 12715];
+    var recovered = [
+      320,
+      3095,
+      26,
+      737,
+      1724,
+      20126,
+      257,
+      118,
+      255,
+      11,
+      5,
+      12715,
+    ];
+
     return (
       <React.Fragment>
+        <div id="div1" style={{ backgroundColor: "#121212" }}>
+          <Plot
+            data={[
+              {
+                type: "scattermapbox",
+                lon: [
+                  -106.4509,
+                  -116.5765,
+                  -63.4168,
+                  -63.7443,
+                  -127.6476,
+                  -73.5491,
+                  -57.6604,
+                  -66.4619,
+                  -98.8139,
+                  -135,
+                  -124.8457,
+                  -85.3232,
+                ],
+                lat: [
+                  52.9399,
+                  53.9333,
+                  46.5107,
+                  44.6819999999999,
+                  53.7267,
+                  52.9399,
+                  53.1355,
+                  46.5653,
+                  53.7609,
+                  64.2823,
+                  64.8255,
+                  51.2538,
+                ],
+                text: [
+                  "Saskatchewan, Canada",
+                  "Alberta, Canada",
+                  "Prince Edward Island, Canada",
+                  "Nova Scotia, Canada",
+                  "British Columbia, Canada",
+                  "Quebec, Canada",
+                  "Newfoundland and Labrador, Canada",
+                  "New Brunswick, Canada",
+                  "Manitoba, Canada",
+                  "Yukon, Canada",
+                  "Northwest Territories, Canada",
+                  "Ontario, Canada",
+                ],
+                customdata: [confirmed, deaths, recovered],
+                mode: "markers",
+                marker: {
+                  size: confirmed / 5000,
+                  color: "red",
+                  opacity: 0.5,
+                },
+                hovertemplate:
+                  "<b>%{text}</b><br><br> Confirmed: %{customdata[0]}<br>Deaths : %{customdata[1]}<br>Recovered : %{customdata[2]}<br> <extra></extra>",
+                // text: ["Montreal"],
+              },
+            ]}
+            layout={{
+              autosize: true,
+              hovermode: "closest",
+              mapbox: {
+                bearing: 0,
+                style: "carto-darkmatter",
+                accesstoken:
+                  "pk.eyJ1Ijoia2VlcnRoaWthcmFqdmVsIiwiYSI6ImNrOWJzd2V3NTAxNDUzbm10enlvdmd4dnUifQ.ylVgtAuLHhOCHYFica7gxA",
+                pitch: 0,
+                zoom: 1,
+              },
+            }}
+          />
+        </div>
+
         <div id="div2" style={{ backgroundColor: "#121212" }}>
           <Plot
             data={[
