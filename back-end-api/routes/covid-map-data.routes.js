511c99da634621c35bcb1a351e220dcb30e60bc4
Krishna Subburaj <shsu1807@colorado.edu>
2020-04-22 23:21:34 -0600

    fixed covid-map-data route

--- back-end-api/routes/covid-map-data.routes.js
+++ back-end-api/routes/covid-map-data.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
     const covidMapData = require("../controllers/covid-map-data.controller.js");
     // Retrieve counts for all dates
-    app.get("/covid-map-data", covidMapData.getAll);
+    app.get("/covid-map-data", covidMapData.getLastUpdated);
   };
