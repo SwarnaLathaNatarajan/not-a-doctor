7d64d21b537089f39b155821c8b02a99da3b14d8
Swarnalatha Natarajan <35426480+SwarnaLathaNatarajan@users.noreply.github.com>
2020-04-24 20:00:52 -0600

    Delete resource-tracker.routes.js

--- back-end-api/routes/resource-tracker.routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) => {
-  const countInfo = require("../controllers/resource-tracker.controller.js");
-  // Retrieve counts for all dates
-  app.get("/resource-tracker", countInfo.getCountsBetweenDates);
-};
