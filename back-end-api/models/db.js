2a1d8270a77610778e05791e328688a0a3c70a29
Aditi <ap2205@gmail.com>
2020-04-26 20:57:01 -0600

    search-box ui completed

--- back-end-api/models/db.js
+++ back-end-api/models/db.js
@@ -1,6 +1,6 @@
 const { BigQuery } = require("@google-cloud/bigquery");
 const projectId = "not-a-doctor-273222";
-const keyFilename = "./models/config/not-a-doctor-273222-aded9c325661.json";
+const keyFilename = "./models/config/not-a-doctor-273222-9a8c7f7e9340.json";
 const bigqueryClient = new BigQuery({ projectId, keyFilename });
 
 module.exports = bigqueryClient;
--- front-end-app/src/components/Searchbox.jsx
+++ front-end-app/src/components/Searchbox.jsx
@@ -1,22 +1,64 @@
 import React, { Component } from "react";
-import ReactSearchBox from "react-search-box";
+import "./search-style.css";
+import { render } from "react-dom";
+import { TransitionMotion, spring } from "react-motion";
 
 class Searchbox extends Component {
-  state = {};
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      active: (props.locked && props.active) || false,
+      value: props.value || "",
+      error: props.error || "",
+      label: props.label || "Search Product"
+    };
   }
+    changeValue(event) {
+      const value = event.target.value;
+      this.setState({ value, error: "" });
+    }
+  
+    handleKeyPress(event) {
+      if (event.which === 13) {
+        console.log('value',event.target.value);
+        
+      }
+    }
+    
+  
   render() {
+    const { active, value, error, label } = this.state;
+    const { predicted, locked } = this.props;
+    const fieldClassName = `field ${(locked ? active : active || value) &&
+      "active"} ${locked && !active && "locked"}`;
+
     return (
-      <div id="searchbox" style={{ width: "50%" }}>
-        <ReactSearchBox
-          placeholder="Search an item"
-          value=""
-          inputBoxBorderColor="#03DAC5"
+      <div className={fieldClassName}>
+        {active &&
+          value &&
+          predicted &&
+          predicted.includes(value) && <p className="predicted">{predicted}</p>}
+        <input
+          id={1}
+          type="text"
+          value={value}
+          placeholder={label}
+          onChange={this.changeValue.bind(this)}
+          onKeyPress={this.handleKeyPress}
+          onFocus={() => !locked && this.setState({ active: true })}
+          onBlur={() => !locked && this.setState({ active: false })}
         />
+        <label htmlFor={1} className={error && "error"}>
+          {error || label}
+        </label>
       </div>
+
     );
   }
 }
 
 export default Searchbox;
+
+
