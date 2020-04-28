b696b879ef591cce18777222571c972121a207c1
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-27 20:52:09 -0600

    Trend analysis module done

--- front-end-app/src/components/Mediatrend.jsx
+++ front-end-app/src/components/Mediatrend.jsx
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Plot from "react-plotly.js";
-import Mediatrendchart from "./Mediatrendchart";
-import { Tabs } from "@yazanaabed/react-tabs";
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 class Mediatrend extends Component {
   constructor(props) {
     super(props);
@@ -62,188 +61,265 @@ class Mediatrend extends Component {
     });
 
     return (
-      <div id="datepickers">
-        <DatePicker
-          dateFormat="MM-dd-yyyy"
-          selected={this.state.startDate}
-          onChange={(date) =>
-            this.setState({
-              startDate: date,
-            })
-          }
-          className="datepicker"
-          maxDate={this.state.endDate}
-        />
-        <DatePicker
-          dateFormat="MM-dd-yyyy"
-          selected={this.state.endDate}
-          onChange={(date) =>
-            this.setState({
-              endDate: date,
-            })
-          }
-          className="datepicker"
-          minDate={this.state.startDate}
-        />
-        <button
-          onClick={this.handleClick(
-            this.parseDate(this.state.startDate),
-            this.parseDate(this.state.endDate)
-          )}
-        >
-          Check Trend
-        </button>
-        <div style={{ color: "white" }}>
-          <br></br>
-          {this.parseDate(this.state.startDate)}
-          <br></br>
-          {this.parseDate(this.state.endDate)}
-          <br></br>
-          {/* {JSON.stringify(this.state.data)} */}
+      <div id="trend">
+        <div className="desc">
+          <h1>Social Media Trend Analysis</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque viverra mauris in aliquam sem fringilla ut. Quisque
+            sagittis purus sit amet volutpat consequat mauris nunc.
+          </p>
         </div>
-
-        <div style={{ backgroundcolor: "white" }}>
-          <Tabs
-            activeTab={{
-              id: "Twitter",
-            }}
+        <div id="datepickers">
+          <button
+            id="checkTrend"
+            onClick={this.handleClick(
+              this.parseDate(this.state.startDate),
+              this.parseDate(this.state.endDate)
+            )}
           >
-            <Tabs.Tab id="Twitter" title="Twitter" style={{ color: "white" }}>
-              <div id="trendchart" style={{ padding: 10 }}>
-                <Plot
-                  data={[
-                    {
-                      x: date ? date : 0,
-                      y: tweets ? tweets : 0,
-                      type: "scatter",
-                      mode: "markers+lines",
-                      line: { color: "#F4B000" },
-                      name: "Tweets",
-                    },
-                  ]}
-                  layout={{
-                    width: "700",
-                    height: "400",
-                    plot_bgcolor: "black",
-                    paper_bgcolor: "black",
-                    grid: false,
-                    title: {
-                      text: "COVID worldwide trend over time",
-                      y: 0.9,
-                      x: 0.5,
-                      xanchor: "center",
-                      yanchor: "top",
-                      font: {
-                        color: "#C0C0C0",
-                        size: 20,
-                      },
-                    },
-                    xaxis: {
-                      type: "date",
-                    },
-                    legend: {
-                      x: 1,
-                      y: 1,
-                      traceorder: "normal",
-                      font: {
-                        size: 12,
-                        color: "white",
-                      },
-                    },
-                  }}
-                />
-              </div>
-            </Tabs.Tab>
-            <Tabs.Tab id="Reddit" title="Reddit" style={{ color: "white" }}>
-              <Plot
-                data={[
-                  {
-                    x: date ? date : 0,
-                    y: reddit ? reddit : 0,
-                    type: "scatter",
-                    mode: "markers+lines",
-                    line: { color: "#F4B000" },
-                    name: "Tweets",
-                  },
-                ]}
-                layout={{
-                  width: "700",
-                  height: "400",
-                  plot_bgcolor: "black",
-                  paper_bgcolor: "black",
-                  grid: false,
-                  title: {
-                    text: "COVID worldwide trend over time",
-                    y: 0.9,
-                    x: 0.5,
-                    xanchor: "center",
-                    yanchor: "top",
-                    font: {
-                      color: "#C0C0C0",
-                      size: 20,
-                    },
-                  },
-                  xaxis: {
-                    type: "date",
-                  },
-                  legend: {
-                    x: 1,
-                    y: 1,
-                    traceorder: "normal",
-                    font: {
-                      size: 12,
-                      color: "white",
-                    },
-                  },
-                }}
-              />
-            </Tabs.Tab>
-            <Tabs.Tab id="Google" title="Google" style={{ color: "white" }}>
-              <Plot
-                data={[
-                  {
-                    x: date ? date : 0,
-                    y: google ? google : 0,
-                    type: "scatter",
-                    mode: "markers+lines",
-                    line: { color: "#F4B000" },
-                    name: "Tweets",
-                  },
-                ]}
-                layout={{
-                  width: "700",
-                  height: "400",
-                  plot_bgcolor: "black",
-                  paper_bgcolor: "black",
-                  grid: false,
-                  title: {
-                    text: "COVID worldwide trend over time",
-                    y: 0.9,
-                    x: 0.5,
-                    xanchor: "center",
-                    yanchor: "top",
-                    font: {
-                      color: "#C0C0C0",
-                      size: 20,
-                    },
-                  },
-                  xaxis: {
-                    type: "date",
-                  },
-                  legend: {
-                    x: 1,
-                    y: 1,
-                    traceorder: "normal",
-                    font: {
-                      size: 12,
-                      color: "white",
-                    },
-                  },
-                }}
-              />
-            </Tabs.Tab>
-          </Tabs>
+            Check Trend
+          </button>
+          <div id="topicker">
+            To
+            <DatePicker
+              dateFormat="MM-dd-yyyy"
+              selected={this.state.endDate}
+              onChange={(date) =>
+                this.setState({
+                  endDate: date,
+                })
+              }
+              className="datepicker"
+              minDate={this.state.startDate}
+            />
+          </div>
+          <div id="frompicker">
+            From
+            <DatePicker
+              dateFormat="MM-dd-yyyy"
+              selected={this.state.startDate}
+              onChange={(date) =>
+                this.setState({
+                  startDate: date,
+                })
+              }
+              className="datepicker"
+              maxDate={this.state.endDate}
+            />
+          </div>
         </div>
+        <Tabs TabIndicatorProps={{ style: { background: "black" } }}>
+          <TabList>
+            <Tab id="Twitter">
+              {" "}
+              <img
+                src={require("../images/twitter.png")}
+                alt="Twitter"
+                height="40"
+                width="40"
+              />
+            </Tab>
+            <Tab id="Reddit">
+              {" "}
+              <img
+                src={require("../images/reddit.png")}
+                alt="Reddit"
+                height="40"
+                width="40"
+              />
+            </Tab>
+            <Tab id="Google">
+              <img
+                src={require("../images/google.png")}
+                alt="Google"
+                height="40"
+                width="40"
+              />
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <Plot
+              data={[
+                {
+                  x: date ? date : 0,
+                  y: tweets ? tweets : 0,
+                  type: "scatter",
+                  mode: "markers+lines",
+                  line: { color: "#03dac5" },
+                  name: "Tweets",
+                },
+              ]}
+              layout={{
+                width: 1700,
+                height: 600,
+                plot_bgcolor: "black",
+                paper_bgcolor: "black",
+                grid: false,
+                title: {
+                  text: "Twitter trend over time",
+                  y: 0.9,
+                  x: 0.5,
+                  xanchor: "center",
+                  yanchor: "top",
+                  font: {
+                    color: "#C0C0C0",
+                    size: 20,
+                  },
+                },
+                xaxis: {
+                  type: "date",
+                  title: "Time",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                yaxis: {
+                  title: "Count",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                hoverlabel: {
+                  font: {
+                    size: 20,
+                  },
+                },
+              }}
+            />
+          </TabPanel>
+          <TabPanel>
+            <Plot
+              data={[
+                {
+                  x: date ? date : 0,
+                  y: reddit ? reddit : 0,
+                  type: "scatter",
+                  mode: "markers+lines",
+                  line: { color: "#ff0266" },
+                  name: "Reddit submissions",
+                },
+              ]}
+              layout={{
+                width: 1700,
+                height: 600,
+                plot_bgcolor: "black",
+                paper_bgcolor: "black",
+                grid: false,
+                title: {
+                  text: "Reddit trend over time",
+                  y: 0.9,
+                  x: 0.5,
+                  xanchor: "center",
+                  yanchor: "top",
+                  font: {
+                    color: "#C0C0C0",
+                    size: 20,
+                  },
+                },
+                xaxis: {
+                  type: "date",
+                  title: "Time",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                yaxis: {
+                  title: "Count",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                hoverlabel: {
+                  font: {
+                    size: 20,
+                  },
+                },
+              }}
+            />
+          </TabPanel>
+          <TabPanel>
+            <Plot
+              data={[
+                {
+                  x: date ? date : 0,
+                  y: google ? google : 0,
+                  type: "scatter",
+                  mode: "markers+lines",
+                  line: { color: "#923ff7" },
+                  name: "Google search",
+                },
+              ]}
+              layout={{
+                width: 1700,
+                height: 600,
+                plot_bgcolor: "black",
+                paper_bgcolor: "black",
+                grid: false,
+                title: {
+                  text: "Google search trend over time",
+                  y: 0.9,
+                  x: 0.5,
+                  xanchor: "center",
+                  yanchor: "top",
+                  font: {
+                    color: "#C0C0C0",
+                    size: 20,
+                  },
+                },
+                xaxis: {
+                  type: "date",
+                  title: "Time",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                yaxis: {
+                  title: "Count",
+                  titlefont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                  tickfont: {
+                    size: 15,
+                    color: "#C0C0C0",
+                  },
+                },
+                hoverlabel: {
+                  font: {
+                    size: 20,
+                  },
+                },
+              }}
+            />
+          </TabPanel>{" "}
+        </Tabs>
       </div>
     );
   }
