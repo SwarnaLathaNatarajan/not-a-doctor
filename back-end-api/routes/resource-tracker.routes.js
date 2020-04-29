6db27e825449846ecc9d558998fbdd3650e390af
Krishna Subburaj <shsu1807@colorado.edu>
2020-04-28 21:35:07 -0600

    revert revert backend api route

--- back-end-api/routes/resource-tracker.routes.js
+++ back-end-api/routes/resource-tracker.routes.js
@@ -1,4 +1,4 @@
 module.exports = (app) => {
   const resourceInfo = require("../controllers/resource-tracker.controller.js");
-  app.get("/resource-tracker", resourceInfo.getResourceInfo);
+  app.get("/api/resource-tracker", resourceInfo.getResourceInfo);
 };
