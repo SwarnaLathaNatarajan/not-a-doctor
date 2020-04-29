b27cb994f0e81d9f929daa8750d1d9e111657ef7
Aditi <ap2205@gmail.com>
2020-04-28 21:10:20 -0600

    landing-page complete

--- front-end-app/src/components/landingpage.js
+++ front-end-app/src/components/landingpage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl'
+import InnerBgImg from "../images/COVID-19.png";
 
+   
 class Landing extends Component{
     render(){
         return(
             <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
+                <Grid className="landing-grid" style={{backgroundImage: "url(" + InnerBgImg +")"}}>
                     <Cell col={12}>
+                        
                         <div className="banner-text">
                             <h1>Not-a-doctor</h1>
                             <hr/>
