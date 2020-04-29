73325249c67c6f891f64402af8b844b9a6ff2c77
Krishna Subburaj <shsu1807@colorado.edu>
2020-04-28 18:41:11 -0600

    revoked route changes in backend api

--- back-end-api/routes/count-info.routes.js
+++ back-end-api/routes/count-info.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
     const countInfo = require("../controllers/count-info.controller.js");
     // Retrieve counts for all dates
-    app.get("/api/count-info", countInfo.getCountsBetweenDates);
+    app.get("/count-info", countInfo.getCountsBetweenDates);
   };
--- back-end-api/routes/covid-map-data.routes.js
+++ back-end-api/routes/covid-map-data.routes.js
@@ -1,11 +1,11 @@
 module.exports = app => {
     const covidMapData = require("../controllers/covid-map-data.controller.js");
     // Retrieve map data for last update
-    app.get("/api/covid-map-data/last-updated", covidMapData.getLastUpdated);
+    app.get("/covid-map-data/last-updated", covidMapData.getLastUpdated);
 
     // Retrieve aggregate map data for last update
-    app.get("/api/covid-map-data/last-updated-aggregates", covidMapData.getLastUpdatedAggregates);
+    app.get("/covid-map-data/last-updated-aggregates", covidMapData.getLastUpdatedAggregates);
 
     // Retrieve aggregate map data for last update
-    app.get("/api/covid-map-data/aggregates", covidMapData.getAggregates);
+    app.get("/covid-map-data/aggregates", covidMapData.getAggregates);
   };
